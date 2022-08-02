ea61e56a40021eb1c3d76a756f4c89943bd9a045
NagypalMarton <nagypalmarton@outlook.com>
2022-08-02 17:32:42 +0200

    newBegin v0.2.5

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let WriterAuthor = []
+
 function getData() {
     let randomID
     try {
@@ -34,20 +36,97 @@ let kivGomb
 function searchBtn(schBtn) {
     let searchText = schBtn.parentElement.firstElementChild.value
     alert(`Ez egy kereső mező lesz! Beírt szöveg: ${searchText}`)
-    kivGomb=schBtn
+    kivGomb = schBtn
     // console.log(kivGomb)
 }
 
 function konyvSzerzoKeres(konyvKeresBtn) {
-    // console.log(konyvKeresBtn)
-    alert('Könyvet vagy Szerzőt fogok keresni!')
-    kivGomb=konyvKeresBtn
+    // alert('Könyvet vagy Szerzőt fogok keresni!')
+    kivGomb = konyvKeresBtn
     //console.log(kivGomb)
+    let keresendoSzoveg = konyvKeresBtn.parentElement.firstElementChild.firstElementChild.lastElementChild.value
+    //console.log(keresendoSzoveg)
+    let fetchInit = {
+        method: "GET", //CRUD => Create(POST) Read(GET) Update(PUT) Delete(DELETE)
+        headers: new Headers(),
+        mode: "cors",
+        cache: "default"
+    }; fetch(`https://moly.hu/api/books.json?q=${keresendoSzoveg}&key=4f7679f7ec5f95a5c7f08da35c79df1b`, fetchInit).then(
+        data => data.json(),
+        err => alert(err)
+    ).then(
+        WriterAuthor => {
+            //console.log(WriterAuthor['books'])
+            createTableCheckedWriterAuthor(WriterAuthor['books'])//addEventListener-rel nem kellene megcsinálni?
+        }
+    );
 }
 
 function szerzoKeres(szerzoKeresBtn) {
     // console.log(szerzoKeresBtn)
     alert('Szerzőt fogok keresni!')
-    kivGomb=szerzoKeresBtn
+    kivGomb = szerzoKeresBtn
     // console.log(kivGomb)
+}
+
+function createTableCheckedWriterAuthor(WriterAuthor) {
+    let table = document.querySelector('#CheckedWriterAuthor')
+
+    //Korábbi Table törlése
+    let tableBody = table.querySelector('tbody')
+    let tableHead = table.querySelector('thead')
+
+    if (isNaN(tableBody) && isNaN(tableHead)) {
+        table.removeChild(tableBody)
+        table.removeChild(tableHead)
+    }
+
+    //caption elkészítése
+    let captionCr = document.createElement("caption")
+    captionCr.innerHTML = "Szerző és művük listázása"
+    table.appendChild(captionCr)
+
+    //thead elkészítése
+    let theadCreate = document.createElement("thead")
+    theadCreate.className = "table-light"
+    let tr = document.createElement("tr")
+    for (let i = 0; i < 3; i++) {
+        let th = document.createElement("th")
+        th.setAttribute("scope", "col")
+        switch (i) {
+            case 0:
+                th.innerHTML = "#"
+                break;
+            case 1:
+                th.innerHTML = "Szerző neve"
+                break;
+            case 2:
+                th.innerHTML = "Kötet címe"
+                break;
+        }
+        tr.appendChild(th)
+    }
+    theadCreate.appendChild(tr)
+    table.appendChild(theadCreate)
+
+    //tbody elkészítése
+    let tbodyCreate = document.createElement("tbody")
+    tbodyCreate.className = "table-group-divider"
+
+    for (let k in WriterAuthor) {
+        let tr = document.createElement("tr")
+        let th = document.createElement("th")
+        th.innerHTML = parseInt(k) + 1
+        th.setAttribute("scope", "row")
+        tr.appendChild(th)
+        for (let value of Object.values(WriterAuthor[k])) {
+            if (typeof (value) != typeof (1)) {//value != k
+                let td = document.createElement("td")
+                td.innerHTML = value
+                tr.appendChild(td)
+            }
+        }
+        tbodyCreate.appendChild(tr)
+        table.appendChild(tbodyCreate)
+    }
 }
